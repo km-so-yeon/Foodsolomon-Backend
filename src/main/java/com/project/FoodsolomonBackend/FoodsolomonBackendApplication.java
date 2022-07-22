e31d6342f2ee6acb85df8dc7197746e7e3852655
DanielYY95 <asrs02041@naver.com>
2022-07-23 01:46:19 +0900

    Revert "회원가입 90% 진행(유효성 검사 진행 중)"
    This reverts commit 1dc041afaba0133eac66578d13be66bd37d329cd.

--- src/main/java/com/project/FoodsolomonBackend/FoodsolomonBackendApplication.java
+++ src/main/java/com/project/FoodsolomonBackend/FoodsolomonBackendApplication.java
@@ -2,8 +2,9 @@ package com.project.FoodsolomonBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//db연결시 (exclude) 삭제
 public class FoodsolomonBackendApplication {
 
 	public static void main(String[] args) {
