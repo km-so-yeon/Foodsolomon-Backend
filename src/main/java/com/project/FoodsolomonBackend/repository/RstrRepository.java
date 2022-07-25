0d868c07733016d8f764193bb0a4247af74c18fc
km-so-yeon <yeonsokm@gmail.com>
2022-07-25 23:11:49 +0900

    fix: 파일구조 수정

--- src/main/java/com/project/FoodsolomonBackend/repository/RstrRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.FoodsolomonBackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.FoodsolomonBackend.entity.Rstr;
-
-public interface RstrRepository  extends JpaRepository<Rstr, Long>{
-
-}
